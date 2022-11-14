1a3c44137fae0d8c5ff8aae4d05426d3a5285ea3
Bryannn <107366548+bryansdream@users.noreply.github.com>
2022-11-14 23:45:06 +0700

    Update ImageCollection.js

--- pages/components/ImageCollection.js
+++ pages/components/ImageCollection.js
@@ -1,24 +1,20 @@
-import { Grid, GridItem } from "@chakra-ui/react"
+import { Grid, GridItem, SimpleGrid } from "@chakra-ui/react"
 import Imaged from "./Imaged"
 import Imaged2 from "./Imaged2"
 
 export default function ImageCollection() {
     return(
         <div>
-        <Grid templateColumns="repeat(4, 1fr)" gap={40}>
-          <GridItem w="100%" h="10">
-            <Imaged image="https://www.outdated.site/_next/image?url=https%3A%2F%2Fxoutdated.s3.ap-south-1.amazonaws.com%2Foutdated%2Fassets%2F1.jpg&w=1920&q=75" />
-          </GridItem>
-          <GridItem w="100%" h="10">
+        <SimpleGrid minChildWidth="400px" columns={4} spacing="100px">
+            <Imaged image="https://www.outdated.site/_next/image?url=https%3A%2F%2Fxoutdated.s3.ap-south-1.amazonaws.com%2Foutdated%2Fassets%2Fslide-3.jpg&w=1920&q=75"/>
             <Imaged2 image="https://www.outdated.site/_next/image?url=https%3A%2F%2Fxoutdated.s3.ap-south-1.amazonaws.com%2Foutdated%2Fassets%2Fslide-3.jpg&w=1920&q=75" />
-          </GridItem>
-          <GridItem w="100%" h="10">
-            <Imaged2 image="https://www.outdated.site/_next/image?url=https%3A%2F%2Fxoutdated.s3.ap-south-1.amazonaws.com%2Foutdated%2Fassets%2Fblacktab.png&w=1920&q=75" />
-          </GridItem>
-          <GridItem w="100%" h="10">
-            <Imaged image="https://www.outdated.site/_next/image?url=https%3A%2F%2Fxoutdated.s3.ap-south-1.amazonaws.com%2Foutdated%2Fassets%2FE5IKOMdVUAMSO8F.jfif&w=1920&q=75" />
-          </GridItem>
-        </Grid>
+            <Imaged2 image="https://www.outdated.site/_next/image?url=https%3A%2F%2Fxoutdated.s3.ap-south-1.amazonaws.com%2Foutdated%2Fassets%2Fslide-3.jpg&w=1920&q=75" />
+            <Imaged image="https://www.outdated.site/_next/image?url=https%3A%2F%2Fxoutdated.s3.ap-south-1.amazonaws.com%2Foutdated%2Fassets%2Fslide-3.jpg&w=1920&q=75" />
+            <Imaged image="https://www.outdated.site/_next/image?url=https%3A%2F%2Fxoutdated.s3.ap-south-1.amazonaws.com%2Foutdated%2Fassets%2Fslide-3.jpg&w=1920&q=75"/>
+            <Imaged2 image="https://www.outdated.site/_next/image?url=https%3A%2F%2Fxoutdated.s3.ap-south-1.amazonaws.com%2Foutdated%2Fassets%2Fslide-3.jpg&w=1920&q=75" />
+            <Imaged2 image="https://www.outdated.site/_next/image?url=https%3A%2F%2Fxoutdated.s3.ap-south-1.amazonaws.com%2Foutdated%2Fassets%2Fslide-3.jpg&w=1920&q=75" />
+            <Imaged image="https://www.outdated.site/_next/image?url=https%3A%2F%2Fxoutdated.s3.ap-south-1.amazonaws.com%2Foutdated%2Fassets%2Fslide-3.jpg&w=1920&q=75" />
+        </SimpleGrid>
       </div>
     )
 }
