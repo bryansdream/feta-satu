32213d6b55b94f62fa76b00ff7a22ebaa2da0633
Bryannn <107366548+bryansdream@users.noreply.github.com>
2022-11-14 23:45:07 +0700

    Update Imaged.js

--- pages/components/Imaged.js
+++ pages/components/Imaged.js
@@ -3,15 +3,13 @@ import { motion } from "framer-motion"
 
 export default function Imaged(props) {
   return (
-    <Box w="fit-content">
+    <Box boxSize={[300, 400, 500]}>
     <motion.div
     initial={{ opacity: 0, rotate: 2, scale: 1 }}
     animate={{ opacity: 1, rotate: 2, scale: 0.9 }}
-    whileHover={{ rotate: 0, scale: 1 }}
-    whileTap={{ rotate: 2 }}>
-      <Box boxSize="fit-content" maxW="unset">
-        <Image src={props.image} alt="Dan Abramov" />
-      </Box>
+    whileHover={{ rotate: 0, scale: 1, }}
+    whileTap={{ rotate: 2, scale: 0.9 }}>
+      <img src={props.image} alt=""/>
     </motion.div>
     </Box>
   );
