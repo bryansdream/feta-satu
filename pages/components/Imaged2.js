32129be71a9cabfe0f189a2b92233dc37727c5fb
Bryannn <107366548+bryansdream@users.noreply.github.com>
2022-11-14 23:45:09 +0700

    Update Imaged2.js

--- pages/components/Imaged2.js
+++ pages/components/Imaged2.js
@@ -1,18 +1,17 @@
-import { Box, Image } from "@chakra-ui/react"
-import { motion } from "framer-motion"
+import { Box, Image } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
 export default function Imaged2(props) {
   return (
-    <Box w="fit-content">
-    <motion.div
-    initial={{ opacity: 0, rotate: -2, scale: 1 }}
-    animate={{ opacity: 1, rotate: -2, scale: 0.9 }}
-    whileHover={{ rotate: 0, scale: 1 }}
-    whileTap={{ rotate: -2 }}>
-      <Box boxSize="fit-content">
+    <Box boxSize={[300, 400, 500]}>
+      <motion.div
+        initial={{ opacity: 0, rotate: -2, scale: 1 }}
+        animate={{ opacity: 1, rotate: -2, scale: 0.9 }}
+        whileHover={{ rotate: 0, scale: 1 }}
+        whileTap={{ rotate: -2, scale: 0.9 }}
+      >
         <Image src={props.image} alt="Dan Abramov" />
-      </Box>
-    </motion.div>
+      </motion.div>
     </Box>
   );
 }
