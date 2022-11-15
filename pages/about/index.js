5c0e562791e4fd4d87823ab7198d046de3a43b09
Bryannn <107366548+bryansdream@users.noreply.github.com>
2022-11-15 09:53:14 +0700

    Create index.js

---
+++ pages/about/index.js
@@ -0,0 +1,15 @@
+import { Box } from "@chakra-ui/react";
+import Link from "next/link";
+
+export default function AboutPage() {
+  return (
+    <div>
+      <p className="font-semibold text-2xl">Contact @</p>
+      <Box boxSize="fit-content" bg="grey" rounded="base" padding="2">
+        <Link href="https://instagram.com/tellmewhenitsready" target="_blank">
+          insta
+        </Link>
+      </Box>
+    </div>
+  );
+}
