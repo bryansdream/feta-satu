da5c50531481fb642e0f9a583468144a9355d926
Bryannn <107366548+bryansdream@users.noreply.github.com>
2022-11-15 09:19:20 +0700

    Update Navbar.js

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -3,7 +3,7 @@ import ThemeToggleButton from "./themeToggleButton";
 
 export default function Navbar() {
   return (
-    <div className="flex font-semibold p-4 text-2xl">
+    <div className="flex font-semibold p-4 text-2xl fixed z-10 bg-white bg-opacity-50 bottom-0">
       <div className="px-2">
         <Link href="/about">about</Link>
       </div>
