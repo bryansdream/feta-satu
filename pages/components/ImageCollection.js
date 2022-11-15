f64ceef896b4b412167b435f362b098cc015b646
Bryannn <107366548+bryansdream@users.noreply.github.com>
2022-11-15 09:19:19 +0700

    Update ImageCollection.js

--- pages/components/ImageCollection.js
+++ pages/components/ImageCollection.js
@@ -6,14 +6,14 @@ export default function ImageCollection() {
     return(
         <div>
         <SimpleGrid minChildWidth="400px" columns={4} spacing="100px">
-            <Imaged image="https://www.outdated.site/_next/image?url=https%3A%2F%2Fxoutdated.s3.ap-south-1.amazonaws.com%2Foutdated%2Fassets%2Fslide-3.jpg&w=1920&q=75"/>
-            <Imaged2 image="https://www.outdated.site/_next/image?url=https%3A%2F%2Fxoutdated.s3.ap-south-1.amazonaws.com%2Foutdated%2Fassets%2Fslide-3.jpg&w=1920&q=75" />
-            <Imaged2 image="https://www.outdated.site/_next/image?url=https%3A%2F%2Fxoutdated.s3.ap-south-1.amazonaws.com%2Foutdated%2Fassets%2Fslide-3.jpg&w=1920&q=75" />
-            <Imaged image="https://www.outdated.site/_next/image?url=https%3A%2F%2Fxoutdated.s3.ap-south-1.amazonaws.com%2Foutdated%2Fassets%2Fslide-3.jpg&w=1920&q=75" />
-            <Imaged image="https://www.outdated.site/_next/image?url=https%3A%2F%2Fxoutdated.s3.ap-south-1.amazonaws.com%2Foutdated%2Fassets%2Fslide-3.jpg&w=1920&q=75"/>
-            <Imaged2 image="https://www.outdated.site/_next/image?url=https%3A%2F%2Fxoutdated.s3.ap-south-1.amazonaws.com%2Foutdated%2Fassets%2Fslide-3.jpg&w=1920&q=75" />
-            <Imaged2 image="https://www.outdated.site/_next/image?url=https%3A%2F%2Fxoutdated.s3.ap-south-1.amazonaws.com%2Foutdated%2Fassets%2Fslide-3.jpg&w=1920&q=75" />
-            <Imaged image="https://www.outdated.site/_next/image?url=https%3A%2F%2Fxoutdated.s3.ap-south-1.amazonaws.com%2Foutdated%2Fassets%2Fslide-3.jpg&w=1920&q=75" />
+            <Imaged image="https://i.ibb.co/0fTdLLm/267956282-434437958327370-1551720664197714667-n.jpg"/>
+            <Imaged2 image="https://i.ibb.co/GPYV0Yw/268593438-625183908931981-9209380790821994144-n.jpg" />
+            <Imaged2 image="https://i.ibb.co/gJjzr80/274319740-1734674310212175-3611442684736990365-n.jpg" />
+            <Imaged image="https://i.ibb.co/b7wWjsd/274182658-412754183942704-7348495299691721582-n.jpg" />
+            <Imaged image="https://i.ibb.co/zNCjtz3/272972246-529798071690061-6103680218309505855-n.jpg"/>
+            <Imaged2 image="https://i.ibb.co/VNDpXmD/274772598-497235915259580-2833016214001972593-n.jpg" />
+            <Imaged2 image="https://i.ibb.co/64xVPQw/274833111-519568269610480-1888972364534261432-n.jpg" />
+            <Imaged image="https://i.ibb.co/HDv9Q5k/277317699-858686354855755-8963026574499317009-n.jpg" />
         </SimpleGrid>
       </div>
     )
