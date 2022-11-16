17bf5d1703145f3ae3cc24cd589c1c7bdbec5190
Bryannn <107366548+bryansdream@users.noreply.github.com>
2022-11-16 22:54:32 +0700

    Update Imaged2.js

--- pages/components/Imaged2.js
+++ pages/components/Imaged2.js
@@ -3,14 +3,13 @@ import { motion } from "framer-motion";
 
 export default function Imaged2(props) {
   return (
-    <Box boxSize={[300, 400, 500]}>
+    <Box boxSize="fit-content">
       <motion.div
         initial={{ opacity: 0, rotate: -2, scale: 1 }}
         animate={{ opacity: 1, rotate: -2, scale: 0.9 }}
         whileHover={{ rotate: 0, scale: 1 }}
-        whileTap={{ rotate: -2, scale: 0.9 }}
       >
-        <Image src={props.image} alt="Dan Abramov"  objectFit="cover" w="500px" h="500px" />
+        <Image src={props.image} alt="Dan Abramov"  objectFit="cover" w={[200, 300, 400, 500]} h={[300, 350, 400, 600]} />
       </motion.div>
     </Box>
   );
