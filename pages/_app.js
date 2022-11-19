388973975804c5ecf2c3a74daadbba41dcb5a7cb
Bryannn <107366548+bryansdream@users.noreply.github.com>
2022-11-19 17:06:50 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,16 @@ import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.css";
 import FetaHero from "./components/feta";
 import Navbar from "./components/Navbar";
+import ThemeToggleButton from "./components/themeToggleButton";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <div className="flex justify-between p-4">
       <FetaHero />
-      <Component {...pageProps} />
       <Navbar />
+      </div>
+      <Component {...pageProps} />
     </ChakraProvider>
   );
 }
