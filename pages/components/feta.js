f100b75ddce133dda856595a94da9a3b5e40d431
Bryannn <107366548+bryansdream@users.noreply.github.com>
2022-11-19 17:07:03 +0700

    Update feta.js

--- pages/components/feta.js
+++ pages/components/feta.js
@@ -1,12 +1,14 @@
-import { Text } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import Link from "next/link";
 
 export default function FetaHero() {
     return(
-        <div>
+        <div className="flex justify-center">
+            <Box mb={[10, 20, 30]}>
             <Link href="/">
-            <Text fontSize="4xl" as="b">Absurd</Text>
+            <Text fontSize="4xl" as="b">Tell Me When It's Ready</Text>
             </Link>
+            </Box>
         </div>
     )
 }
