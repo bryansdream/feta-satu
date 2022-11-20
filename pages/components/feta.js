9072954e12223b90b3dfce1a83faa7d44222b7b9
Bryannn <107366548+bryansdream@users.noreply.github.com>
2022-11-20 12:37:15 +0700

    Update feta.js

--- pages/components/feta.js
+++ pages/components/feta.js
@@ -1,14 +1,18 @@
 import { Box, Text } from "@chakra-ui/react";
 import Link from "next/link";
+import Tada from 'react-reveal/Tada';
+
 
 export default function FetaHero() {
     return(
         <div className="flex justify-center">
+            <Tada>
             <Box mb={[10, 20, 30]}>
             <Link href="/">
             <Text fontSize="4xl" as="b">Tell Me When Its Ready</Text>
             </Link>
             </Box>
+            </Tada>
         </div>
     )
 }
