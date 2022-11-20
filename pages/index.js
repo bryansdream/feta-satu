10c21a228097f1492414aac3cb07f6ef026fad9b
Bryannn <107366548+bryansdream@users.noreply.github.com>
2022-11-20 12:37:23 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,34 @@
 import { Container, Grid, GridItem } from "@chakra-ui/react";
 import ImageCollection from "./components/ImageCollection";
-import Imaged from "./components/Imaged";
-import Imaged2 from "./components/Imaged2";
+import { useRef, useEffect, useState } from "react";
+import { InView } from "react-intersection-observer";
+import Head from "next/head";
+// import ScrollReveal from 'scrollreveal'
 
 export default function Home() {
+  const myRef = useRef();
+  const [myElementIsVisible, setMyElementIsVisible] = useState();
+  console.log("myElementIsVisible", myElementIsVisible);
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const entry = entries[0];
+      setMyElementIsVisible(entry.isIntersecting);
+    });
+    observer.observe(myRef.current);
+  }, []);
+  // ScrollReveal().reveal('.photo')
   return (
-    <Container maxW="9xl">
-      <ImageCollection/>
-    </Container>
+    <>
+      <Head>
+        <title>Feta</title>
+      </Head>
+      <Container maxW="9xl">
+        <div ref={myRef} className="photo">
+          <ImageCollection />
+        </div>
+        <ImageCollection />
+        <ImageCollection />
+      </Container>
+    </>
   );
 }
