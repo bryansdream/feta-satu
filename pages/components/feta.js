f3d147b09ee12410ea76c321ac7b67cc2dee6c90
Bryannn <107366548+bryansdream@users.noreply.github.com>
2022-11-20 16:12:01 +0700

    Update feta.js

--- pages/components/feta.js
+++ pages/components/feta.js
@@ -5,11 +5,11 @@ import Tada from 'react-reveal/Tada';
 
 export default function FetaHero() {
     return(
-        <div className="flex justify-center">
+        <div className="flex justify-center font-sans">
             <Tada>
             <Box mb={[10, 20, 30]}>
             <Link href="/">
-            <Text fontSize="4xl" as="b">Tell Me When Its Ready</Text>
+            <p className="font-bold text-2xl md:text-3xl lg:text-4xl">Tell Me When Its Ready</p>
             </Link>
             </Box>
             </Tada>
