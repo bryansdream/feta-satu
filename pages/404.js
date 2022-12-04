8241edfb089c55ef5efea80edbb1ad32b6d84354
Bryannn <107366548+bryansdream@users.noreply.github.com>
2022-12-04 12:57:15 +0700

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,7 @@
 export default function ErrorPage() {
     return(
         <div className="text-black text-8xl flex items-center justify-center font-semibold">
-            <p>404 ERROR KONTOL</p>
+            <p>404 ERROR</p>
         </div>
     )
 }
