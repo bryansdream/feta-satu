08e0c8e80a00344428a3a56d75ed56aeb8adfefb
Bryannn <107366548+bryansdream@users.noreply.github.com>
2022-12-04 12:57:19 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,9 @@ export default function Home() {
         <div ref={myRef}>
           <ImageCollection />
         </div>
+        <div ref={myRef}>
+          <ImageCollection />
+        </div>
       </Container>
     </div>
   );
