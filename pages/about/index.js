fa891f6a56b43b65c824f07a7b2f349417773ff8
Bryannn <107366548+bryansdream@users.noreply.github.com>
2022-12-04 12:57:17 +0700

    Update index.js

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,16 +1,23 @@
-import { Box, Icon } from "@chakra-ui/react";
+import { Box, Container, Icon } from "@chakra-ui/react";
 import Link from "next/link";
-import { AiFillInstagram } from "react-icons/ai"
+import { AiFillInstagram, AiFillTwitterSquare } from "react-icons/ai"
 
 export default function AboutPage() {
   return (
     <div>
+      <Container maxW="7xl">
       <p className="font-semibold text-2xl">Hit Me Up @</p>
-      <Box boxSize="fit-content" bg="grey" rounded="base" padding="2">
+      <Box boxSize="fit-content" bg="purple.300" rounded="base" padding="2">
         <Link href="https://instagram.com/tellmewhenitsready" target="_blank">
-          <Icon as={AiFillInstagram} w="6" h="6" />
+          <Icon as={AiFillInstagram} w="6" h="6" color="white" />
         </Link>
       </Box>
+      <Box boxSize="fit-content" bg="blue.300" rounded="base" padding="2">
+        <Link href="https://instagram.com/tellmewhenitsready" target="_blank">
+          <Icon as={AiFillTwitterSquare} w="6" h="6" color="white" />
+        </Link>
+      </Box>
+      </Container>
     </div>
   );
 }
